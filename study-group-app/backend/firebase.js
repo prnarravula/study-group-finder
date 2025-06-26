f85f868d27eaf351c045723ebd5c449826c362d4
Pranav Narravula <pranavnarravula@gmail.com>
2025-06-26 18:15:59 -0400

    Imported Firebase and created starter firebase file under backend

---
+++ study-group-app/backend/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBlxXA86S981r2KmmQ5wZ7UCKhr0xuUKdE",
+  authDomain: "study-group-app-77b68.firebaseapp.com",
+  projectId: "study-group-app-77b68",
+  storageBucket: "study-group-app-77b68.appspot.com",
+  messagingSenderId: "914748936839",
+  appId: "1:914748936839:web:31b4634dfb7e30faf25e22",
+  measurementId: "G-EFSYTYBNSG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
