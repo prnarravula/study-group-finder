7ba4bdb2e2465dab056cd6cc35661faaeaade1f2
Pranav Narravula <pranavnarravula@gmail.com>
2025-06-26 17:47:05 -0400

    Fixed file structure

--- study-group-app/App.js
+++ study-group-app/App.js
@@ -4,20 +4,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import BottomNavBar from './navigation/BottomNavBar';
+import BottomNavBar from './frontend/navigation/BottomNavBar';
 
 //screens
-import CalendarScreen from './screens/CalendarScreen';
-import ChatsScreen from './screens/ChatsScreen';
-import FindGroupScreen from './screens/FindGroupScreen';
-import HomeScreen from './screens/HomeScreen';
-import IndChatScreen from './screens/IndChatScreen';
-import LogInScreen from './screens/LogInScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import ReportScreen from './screens/ReportScreen';
-import SignUpScreen from './screens/SignUpScreen';
-import VerifyEmailScreen from './screens/VerifyEmailScreen';
-import YourGroupsScreen from './screens/YourGroupsScreen';
+import CalendarScreen from './frontend/screens/CalendarScreen';
+import ChatsScreen from './frontend/screens/ChatsScreen';
+import FindGroupScreen from './frontend/screens/FindGroupScreen';
+import HomeScreen from './frontend/screens/HomeScreen';
+import IndChatScreen from './frontend/screens/IndChatScreen';
+import LogInScreen from './frontend/screens/LogInScreen';
+import ProfileScreen from './frontend/screens/ProfileScreen';
+import ReportScreen from './frontend/screens/ReportScreen';
+import SignUpScreen from './frontend/screens/SignUpScreen';
+import VerifyEmailScreen from './frontend/screens/VerifyEmailScreen';
+import YourGroupsScreen from './frontend/screens/YourGroupsScreen';
 
 const Stack = createNativeStackNavigator();
 
