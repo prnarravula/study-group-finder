d9a05d6a65c16c763ece751e9ba4381089c974d0
Pranav Narravula <pranavnarravula@gmail.com>
2025-06-27 16:43:22 -0400

    Configured .env file/babel

--- study-group-app/backend/firebase.js
+++ study-group-app/backend/firebase.js
@@ -1,5 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID
+} from '@env';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -7,13 +16,13 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBlxXA86S981r2KmmQ5wZ7UCKhr0xuUKdE",
-  authDomain: "study-group-app-77b68.firebaseapp.com",
-  projectId: "study-group-app-77b68",
-  storageBucket: "study-group-app-77b68.appspot.com",
-  messagingSenderId: "914748936839",
-  appId: "1:914748936839:web:31b4634dfb7e30faf25e22",
-  measurementId: "G-EFSYTYBNSG"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
